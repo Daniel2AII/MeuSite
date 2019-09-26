92513e011afffc53d81c4e1162b11e71bdd5643d
Daniel Alves Cruz <dalvescruz3002@gmail.com>
2019-09-26 08:54:35 -0300

    Escritor ou Modulo render_templates

--- Site.py
+++ Site.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def Index():
     return render_template("Index.html")
 
+@app.route("/register")
+def Register():
+    return render_template("/_links/_Register.html")
+
+@app.route("/about")
+def About():
+    return render_template("_links/_About.html")
+
+@app.route("/form_doctor")
+def FormDoctor():
+    return render_template("_links/_FormD.html")
+
+@app.route("/form_pacient")
+def FormPacient():
+    return render_template("_links/_FormP.html")
+
 if __name__ == "__main__":
     app.run(debug = True)
