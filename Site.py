243ff1730b452fdd9abc0f52bc672d18068a0b0f
Daniel Alves Cruz <dalvescruz3002@gmail.com>
2019-09-26 10:08:57 -0300

    Escritor ou Modulo render_tempaltes

--- Site.py
+++ Site.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def Index():
     return render_template("Index.html")
 
-@app.route("/register")
+@app.route("/register", methods=['POST'])
 def Register():
     return render_template("/_links/_Register.html")
 
