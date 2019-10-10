7731c29a501e4483d3a0148b51be2f00877523ce
Daniel Alves Cruz <dalvescruz3002@gmail.com>
2019-10-10 09:55:18 -0300

    Modulador do projeto

--- Site.py
+++ Site.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Banco_Usuarios.db'
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 def Index():
@@ -21,6 +25,15 @@ def FormDoctor():
 @app.route("/form_pacient")
 def FormPacient():
     return render_template("_links/_FormP.html")
+
+
+class Pacient(db.Model):
+    usuario = db.Column(db.String(20), primary_key=True)
+    nome = db.Column(db.String(100), unique=True, nullable=True)
+    idade = db.Column(db.Integer, unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    senha = db.Column(db.Integer, unique=True, nullable=False)
+
 
 if __name__ == "__main__":
     app.run(debug = True)
