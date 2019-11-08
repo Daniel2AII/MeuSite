7d070e9876547f340f00d5553070e0bd71b3a5ca
Daniel Alves Cruz <dalvescruz3002@gmail.com>
2019-11-08 09:28:47 -0200

    Modulador de banco de dados

--- Models.py
+++ Models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from app import app
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Banco_Usuarios.db'
 app.config['SECRET_KEY'] = '2f15588715f5012b4d63e76a1a47e357'
@@ -7,7 +8,10 @@ db = SQLAlchemy(app)
 class Pacient(db.Model):
     usuarioP = db.Column(db.String(20), primary_key=True)
     nomeP = db.Column(db.String(100), unique=True, nullable=True)
-    idade = db.Column(db.Integer, unique=True, nullable=False)
+    dataNascimento = db.Column(db.DateTime, default=datetime.utcnow), unique=True, nullable=False
+    e_mailP = db.Column(db.String(100), unique=True nullable=False)
+    sex = db.Column(db.String(20), unique=True nullable=False)
+    senhaP = db.Column(db.String(100), unique=True, nullable=False)
 
     def __repr__(self):
         return '<Pacient %r>' % self.usuarioP
@@ -15,6 +19,8 @@ class Pacient(db.Model):
 class Doctor(db.Model):
     usuarioD = db.Column(db.String(20), primary_key=True)
     nomeD = db.Column(db.String(100), unique=True, nullable=True)
+    e_mailD = db.Column(db.String(100), unique=True nullable=False)
+    senhaD = db.Column(db.String(100), unique=True, nullable=False)
 
     def __repr__(self):
     return '<Doctor %r>' % self.usuarioD
