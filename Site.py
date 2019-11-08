280636f65a897e725ee2a6d34e84ce84b25190a0
Daniel Alves Cruz <dalvescruz3002@gmail.com>
2019-11-08 08:33:44 -0200

    Modulador de rtas

--- Site.py
+++ Site.py
@@ -1,13 +1,6 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from forms.py import FormDoctor
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Banco_Usuarios.db'
-app.config['SECRET_KEY'] = '2f15588715f5012b4d63e76a1a47e357'
-
-db = SQLAlchemy(app)
-
 
 @app.route("/")
 def Index():
@@ -28,22 +21,6 @@ def FormDoctor():
 @app.route("/form_pacient")
 def FormPacient():
     return render_template("_links/_FormP.html")
-
-
-class Pacient(db.Model):
-    usuarioP = db.Column(db.String(20), primary_key=True)
-    nomeP = db.Column(db.String(100), unique=True, nullable=True)
-    idade = db.Column(db.Integer, unique=True, nullable=False)
-
-def __repr__(self):
-    return '<Pacient %r>' % self.usuarioP
-
-class Doctor(db.Model):
-    usuarioD = db.Column(db.String(20), primary_key=True)
-    nomeD = db.Column(db.String(100), unique=True, nullable=True)
-
-def __repr__(self):
-    return '<Doctor %r>' % self.usuarioD
 
 
 if __name__ == "__main__":
