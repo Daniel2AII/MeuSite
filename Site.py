58567a4fb878f86834f80f2c7c95c48d555d151e
Daniel Alves Cruz <dalvescruz3002@gmail.com>
2019-11-08 18:05:28 -0200

    Modulador de rotas

--- Site.py
+++ Site.py
@@ -9,19 +9,19 @@ def Index():
 @app.route("/register", methods=['POST', 'GET'])
 def Register():
 
-    form = FormDoctor()
-    if form.validate_on_submit():
-        user = Doctor()
-        user.usuarioD = form.usuarioD.data
-        db.session.add(user)
-        db.session.commit()
+    #form = FormDoctor()
+    #if form.validate_on_submit():
+    #   user = Doctor()
+    #  user.usuarioD = form.usuarioD.data
+    # db.session.add(user)
+    #db.session.commit()
 
-    form = FormPacient()
-    if form.validate_on_submit():
-        user = Pacient()
-        user.usuarioP = form.usuarioP.data
-        db.session.add(user)
-        db.session.commit()
+    #form = FormPacient()
+    #if form.validate_on_submit():
+    #    user = Pacient()
+    #    user.usuarioP = form.usuarioP.data
+    #    db.session.add(user)
+    #    db.session.commit()
 
     return render_template("/_links/_Register.html")
 
