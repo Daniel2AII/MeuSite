ed792a399f32dbce649d94bb4db30d8e758bf5ea
Daniel Alves Cruz <dalvescruz3002@gmail.com>
2019-11-20 19:10:19 -0200

    Modulador de formulario

--- forms.py
+++ forms.py
@@ -2,9 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BoleanField, DataRequired, Length, Email, EqualTo, DateTimeField
 
 class FormPacient(FlaskForm):
-    usernameP = StringField('Usuário', validators=[DataRequired(), Length(min=2, max=20)])
-    nameP = StringField('Nome', validators=[DataRequired(), Length(min=2, max=100)])
-    emailP = StringField('E-mail', validators=[DataRequired(), Email()])
+    username = StringField('Usuário', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
     birthday = DateTimeField('Data de Nascimento', validators=[DataRequired()])
-    passwordP = PasswordField('Senha', validators=[DataRequired()])
-    confirm_passwordP = PasswordField('Repete Senha', validators[DataRequired(), EqualTo('password')])
+    genre = StringField('Gênero', validators=[DataRequired()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    confirm_password = PasswordField('Repete Senha', validators[DataRequired(), EqualTo('password')])
