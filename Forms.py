ab1575eec21fc16cff62c22c297c7b16e0cff7de
Daniel Alves Cruz <dalvescruz3002@gmail.com>
2019-12-01 06:56:37 -0300

    Modulador de formularios

--- Forms.py
+++ Forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, DateTimeField
+from wtforms import StringField, PasswordField, SubmitField, DateTimeField, BooleanField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class FormPacient(FlaskForm):
@@ -9,3 +9,16 @@ class FormPacient(FlaskForm):
     genre = StringField('Gênero', validators=[DataRequired()])
     password = PasswordField('Senha', validators=[DataRequired()])
     confirm_password = PasswordField('Repete Senha', validators=[DataRequired(), EqualTo('password')])
+
+class Respostas(FlaskForm):
+    usuario = StringField('Usuario', validators=[DataRequired(), Length(min=5, max=50)])
+    resp1 = BooleanField('Resposta 1', validators=[DataRequired()])
+    resp2 = BooleanField('Resposta 2', validators=[DataRequired()])
+    resp3 = BooleanField('Resposta 3', validators=[DataRequired()])
+    resp4 = BooleanField('Resposta 4', validators=[DataRequired()])
+    resp5 = BooleanField('Resposta 5', validators=[DataRequired()])
+    resp6 = BooleanField('Resposta 6', validators=[DataRequired()])
+    resp7 = BooleanField('Resposta 7', validators=[DataRequired()])
+    resp8 = BooleanField('Resposta 8', validators=[DataRequired()])
+    resp_consulta = BooleanField('Consultado', validators=[DataRequired()])
+    Q_tempo = DateTimeField('Quantidade de tempo', validators=[DataRequired()])
